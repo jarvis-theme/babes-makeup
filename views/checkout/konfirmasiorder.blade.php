7622f41953a26798d178fd9d1b67b2ba50651dc6
yogaswara <gistha.yogaswara@gmail.com>
2016-05-19 10:05:03 +0700

    add confirmation ipaymu

--- views/checkout/konfirmasiorder.blade.php
+++ views/checkout/konfirmasiorder.blade.php
@@ -104,6 +104,18 @@
                     <center id="paypal">{{$paypalbutton}}</center>
                     <br>
                 </div>
+            @elseif($order->jenisPembayaran==4) 
+                @if(($checkouttype==1 && $order->status < 2) || ($checkouttype==3 && ($order->status!=6)))
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+                    <center>
+                        <h3>{{trans('content.step5.confirm_btn')}} iPaymu</h3>
+                        <hr>
+                        <p>{{trans('content.step5.ipaymu')}}</p>
+                        <a class="btn btn-info" href="{{url('ipaymu/'.$order->id)}}" target="_blank">{{trans('content.step5.ipaymu_btn')}}</a>
+                    </center>
+                    <br>
+                </div>
+                @endif
             @elseif($order->jenisPembayaran==5 && $order->status == 0)
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2">
                     <center>
